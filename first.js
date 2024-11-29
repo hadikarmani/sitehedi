3fd486e45cc59e94b9887e4645469179c43d6298
unknown <hadikarmani.com@gmail.com>
2024-11-29 11:58:31 +0100

    step1

---
+++ first.js
@@ -0,0 +1,22 @@
+console.log("winoupapa")
+console.log("hi bro")
+
+
+// hedi karmani 
+
+/**dazkdlmd
+ * azdald
+ * 
+*/
+let num1=10
+
+let firstname='hedi'
+let lastname='karmani'
+
+
+
+console.log(num1)
+console.log(firstname)
+console.log(lastname)
+
+console.log(`this is num1 ${firstname}`)
